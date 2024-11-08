d56d334836f27b80cccf2a311237fde7a6b600a8
Escanor-LionSin <100279039+Escanor-LionSin@users.noreply.github.com>
2024-11-08 12:59:08 +0530

    add: Scripts dir

---
+++ Scripts/nmap_commands.sh
@@ -0,0 +1,12 @@
+nmap -sn <target_ip_or_domain>
+nmap -p <port> <target_ip_or_domain>
+nmap -p- <target_ip_or_domain>
+nmap -sV <target_ip_or_domain>
+nmap -O <target_ip_or_domain>
+nmap -A <target_ip_or_domain>
+nmap -T<level> <target_ip_or_domain>
+nmap -sT <target_ip_or_domain>
+nmap -sS <target_ip_or_domain>
+nmap -Pn <target_ip_or_domain>
+nmap --traceroute <target_ip_or_domain>
+
